0cbdc4371099e606fb259e55e001868f321248df
Jose J. Martinez <jose.martinez@wayops.eu>
2023-12-14 20:30:08 +0000

    Prepares release 0.1

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-from distutils.core import setup
-
-from setuptools import find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="mindcraft",
-    version="0.1rc1",
+    version="0.1",
     packages=find_packages(),
     package_data={p: ["*"] for p in find_packages()},
-    url="",
+    url="https://github.com/josejuanmartinez/mindcraft",
+    download_url="https://github.com/josejuanmartinez/mindcraft",
     license="MIT",
     install_requires=[
         "transformers==4.35.2",
@@ -19,5 +18,22 @@ setup(
     python_requires=">=3.10.0",
     author="Juan.Martinez",
     author_email="jjmcarrascosa@gmail.com",
-    description="Mindcraft: an LLM-based engine for creating real NPCs",
+    description="Mindcraft: an LLM-based engine for creating real NPCs. Empowered by Hugging Face, quantized LLMs with "
+                "AWQ (thanks @TheBloke) and vLLM. It follows a RAG approach with chunk or sentence splitting, and a "
+                "vector store. Right now, ChromaDB is the supported Vector Store and chunk splitting using `tiktoken` "
+                "or sentence splitting using `spacy` are available.",
+    keywords=["nlp", "llm", "npc", "conversations", "videogames"],
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Environment :: Console',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Environment :: GPU :: NVIDIA CUDA :: 12 :: 12.1',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+          'Topic :: Games/Entertainment'
+      ]
 )
